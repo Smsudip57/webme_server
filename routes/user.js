093cb1599d31ad03660e14ac6d560b4dba657d38
Sudip Mandal <sudip7981@gmail.com>
2025-01-11 08:38:25 +0600

    sd

--- routes/user.js
+++ routes/user.js
@@ -32,9 +32,9 @@ router.post('/login', async (req, res) => {
     
     const token = jwt.sign({ userId: user._id }, JWT_SECRET);
     res.cookie('user', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      httpOnly: false,
+      secure: false,
+      sameSite: '',
       path: '/',
     });
 
